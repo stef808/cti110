a44c48a81684dbb5fd72c38ba89e18148b0f4915
stef808 <135177067+stef808@users.noreply.github.com>
2023-06-01 18:04:25 -0400

    second submission

---
+++ P1HW1 morales stefan.py
@@ -0,0 +1,11 @@
+def operations_on_numbers():
+    number1 = int(input("Enter integer:"))
+    print(f"\nYou entered: {number1}")
+    print(f"{number1} squared is {number1**2}")
+    print(f"And {number1} cubed is {number1**3} !!")
+
+    number2 = int(input("Enter another integer:"))
+    print(f"\n{number1} + {number2} is {number1 + number2}")
+    print(f"{number1} * {number2} is {number1 * number2}")
+
+operations_on_numbers()
